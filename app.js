d9c946aaa5b8ba4a866a83b79c04cf11c28f8cc6
shadowolfyt <shadowolfbusiness@gmail.com>
2019-06-12 14:13:42 +1000

    Updated the get day to Australia/Sydney Timezone.

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ client.on('ready', async () => {
 // Get the day of year that YouVersion should display
 //
 function getDayOfYear() {
-  var now = new Date();
+  var now = new Date().toLocaleString("en-AU", {timeZone: "Australia/Sydney"});
   var start = new Date(now.getFullYear(), 0, 0);
   var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
   var oneDay = 1000 * 60 * 60 * 24;
